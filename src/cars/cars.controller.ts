8f4c36908ef48e8a1eaf5e0c1f87a86e5f5823c1
Alberto Gonzalez <43555310+T0kiii@users.noreply.github.com>
2023-12-06 18:12:40 +0100

    Services

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,18 +1,27 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-    private cars = ['Toyota','Honda','Jeep'];
-
+    constructor(
+        private readonly carsService: CarsService
+    ){}
+    
     @Get()
     getAllCars(){
-        return this.cars;
+        return this.carsService.findAll();
     }
 
     @Get(':id')
     getCarById( @Param('id') id: string){
         console.log({id});
-        return this.cars[id];
+        return this.carsService.findById(+id);
     }
+
+    /* @Get(':id')
+    getCarById( @Param('id') id: string){
+        console.log({id});
+        return this.cars[id];
+    } */
 }
