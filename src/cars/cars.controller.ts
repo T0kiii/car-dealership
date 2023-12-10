1de10d86768806b401325233829719018900a284
Alberto Gonzalez <43555310+T0kiii@users.noreply.github.com>
2023-12-10 14:42:17 +0100

    Class Validator y Class Transformer

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,41 +1,41 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
+@UsePipes(ValidationPipe)
 export class CarsController {
 
     constructor(
         private readonly carsService: CarsService
-    ){}
-    
+    ) { }
+
     @Get()
-    getAllCars(){
+    getAllCars() {
         return this.carsService.findAll();
     }
 
     @Get(':id')
-    getCarById( @Param('id', ParseUUIDPipe) id: string){
-        console.log({id});
+    getCarById(@Param('id', ParseUUIDPipe) id: string) {
+        console.log({ id });
         return this.carsService.findById(id);
     }
 
     @Post()
-    createCar( @Body() createCarDto: CreateCarDto){
+    createCar(@Body() createCarDto: CreateCarDto) {
         return createCarDto;
     }
 
     @Patch(':id')
-    updateCar( 
-        @Param('id', ParseIntPipe) id: number, 
-        @Body() body: any 
-        )
-        {
-        return {id,body};
+    updateCar(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() body: any
+    ) {
+        return { id, body };
     }
-    
+
     @Delete(':id')
-    deleteCar( @Param('id', ParseIntPipe) id: number){
+    deleteCar(@Param('id', ParseIntPipe) id: number) {
         return id;
     }
 }
--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,4 +1,9 @@
+import { IsString } from "class-validator";
+
 export class CreateCarDto {
+    
+    @IsString( { message: `Mensaje personalizado: Tiene que ser un string`} )
     readonly brand: string;
+    @IsString()
     readonly model: string;
 }
